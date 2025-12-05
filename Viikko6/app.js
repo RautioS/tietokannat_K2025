e46d47f971093bcb39ca08c5dee874128c5863c6
Santeri <santerirautio17@gmail.com>
2025-12-05 13:28:53 +0200

    Tehtävä 19 valmis

--- Viikko6/app.js
+++ Viikko6/app.js
@@ -18,3 +18,50 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+// Yleinen middleware
+app.use(
+    function(req,res,next){
+        console.log('The common middleware called');
+        next();
+    }
+);
+
+// Middleware-esimerkki
+app.use('/example',
+    function(req,res,next){
+        console.log('The example middleware called');
+        next();
+    }
+);
+
+// GET-metodi
+app.get('/example',
+    function(request,response){
+        response.send('I am example');
+        console.log('I am example');
+    }
+);
+
+// GET-metodi yhdellä parametrilla
+app.get('/example/:name',
+    function(request,response){
+        response.send('Hello '+request.params.name);
+    }
+);
+
+//GET-metodi kahdella parametrilla
+app.get('/example2/:firstName/:lastName',
+    function(request, response){
+        response.send('Hello '+request.params.firstName+" "+request.params.lastName);
+    }
+);
+
+// POST-metodi
+app.post('/',
+    function(request,response){
+        response.send(request.body);
+        console.log(request.body);
+    }
+);
+
